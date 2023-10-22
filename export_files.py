8ca6fb289c69e42719b6562ba13a6bb012b37a47
bonzai133 <bonzai133@free.fr>
2023-10-22 17:07:57 +0200

    format

--- export_files.py
+++ export_files.py
@@ -4,6 +4,7 @@ import requests
 # 1. Run Flask app on http://localhost:5000
 # 2. Execute this script
 
+
 def main(hostname, output):
     files = [
         ("home.html", "home.html"),
@@ -20,6 +21,7 @@ def main(hostname, output):
         f2 = open(f"{output}/{file_path}", "w")
         f2.write(my_page)
         f2.close()
+
 
 if __name__ == "__main__":
     hostname = "http://localhost:5000"
